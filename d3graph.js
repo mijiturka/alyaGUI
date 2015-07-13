46064aaed5861df60d7670c3824d6d0a7fd77908
Rumyana Rafailova <s1004618@sms.ed.ac.uk>
2015-07-13 17:24:49 +0100

    camera controls, select from 2d or 3d image

---
+++ d3graph.js
@@ -0,0 +1,270 @@
+ // D3 window
+ function createSVG() {
+     width = window.d3windowWidth;
+     height = window.d3windowHeight;
+     fill = d3.scale.category20();
+
+     // mouse event vars
+     selected_node = null;
+     selected_link = null;
+     selected_mesh = null;
+     mousedown_link = null;
+     mousedown_node = null;
+     mouseup_node = null;
+     mousedown_mesh = null;
+     mouseup_mesh = null;;
+
+
+     // init svg
+     outer = d3.select("#chart")
+         .append("svg:svg")
+         .attr("width", width)
+         .attr("height", height)
+         .attr("pointer-events", "all");
+
+     vis = outer
+         .append('svg:g')
+         .call(d3.behavior.zoom().on("zoom", rescale))
+         .on("dblclick.zoom", null)
+         .append('svg:g')
+         .on("mousemove", mousemove)
+         .on("mousedown", mousedown)
+         .on("mouseup", mouseup);
+
+     vis.append('svg:rect')
+         .attr('width', width)
+         .attr('height', height)
+         .attr('fill', '#F0F0F0');
+
+     // init force layout
+     force = d3.layout.force()
+         .size([width, height])
+         .linkDistance(50)
+         .charge(-5000)
+         .on("tick", tick);
+ }
+
+function updateSVG() {
+    width = window.d3windowWidth;
+    height = window.d3windowHeight;
+    outer.attr("width", width)
+         .attr("height", height)
+    vis.selectAll("rect")
+        .attr('width', width)
+        .attr('height', height);
+    force.size([width, height]);
+}
+
+ function createGraph(graph) {
+     //Remove previous graph if we need to
+     outer.selectAll("link").remove();
+     outer.selectAll("node").remove();
+
+             link = vis.selectAll(".link"),
+             node = vis.selectAll(".node");
+
+             colorby = "NELEM";
+             var valmin = d3.min(function() {
+                 var temp = [];
+                 graph.nodes.forEach(function(d) {
+                     temp.push(d.props[colorby]);
+                 });
+                 return temp
+             }());
+             var valmax = d3.max(function() {
+                 var temp = [];
+                 graph.nodes.forEach(function(d) {
+                     temp.push(d.props[colorby]);
+                 });
+                 return temp
+             }());
+             var GraphColor = d3.scale.linear()
+                 .domain([valmin, valmax])
+                 .range(["#d7191c", "#ffffbf", "#2c7bb6"])
+                 .interpolate(d3.interpolateHcl);
+
+             force
+                 .nodes(graph.nodes)
+                 .links(graph.links);
+
+             link = link.data(graph.links);
+
+             link.enter().append("line")
+                 .attr("class", "link")
+                 .classed("link_selected", function(d) {
+                     return d === selected_link;
+                 })
+                 .on("mousedown",
+                     function(d) {
+                         mousedown_link = d;
+                         if (mousedown_link == selected_link) selected_link = null;
+                         else selected_link = mousedown_link;
+                         selected_node = null;
+                     });
+
+             node = node.data(graph.nodes, function(d) {
+                 return d.id;
+             })
+                 .enter().append("circle")
+                 .attr("class", "node")
+                 .attr("r", 12)
+                 .style("fill", function(d) {
+                     return GraphColor(d.props[colorby]);
+                 })
+                 .on("mousedown",
+                     function(d, i) {
+                         // disable zoom
+                         //vis.call(d3.behavior.zoom().on("zoom"), null);
+                         
+                         mousedown_node = d;
+                         mousedown_mesh = i;
+                         
+                         var is_selected = this.classList.contains("node_selected");
+                         var id = d.index;
+                         
+                         if (is_selected) { 
+                         	deselectNode(id);
+                         	deselectMesh(id);
+                         }
+                         else { 
+                         	selectNode(id);
+                         	selectMesh(id); 
+                         }
+                         
+                         selected_link = null;
+
+                     })
+                 .on("mousedrag",
+                     function(d) {
+                         // redraw();
+                     })
+                 .on("mouseup",
+                     function(d) {
+                     
+                         if (mousedown_node) {
+                             mouseup_node = d;
+                             if (mouseup_node == mousedown_node) {
+                                 resetMouseVars();
+                                 return;
+                             }
+                             // enable zoom
+                             vis.call(d3.behavior.zoom().on("zoom"), rescale);
+                         }
+                     });
+
+             force.start();
+    }
+
+	function deselectNode(id) {
+		
+		var currnode = d3.selectAll(".node_selected");
+	    currnode.classed("node_selected", function(d, i) {
+	       				//deselect only current node
+                       	return d.index != id;
+	               	 	});
+			
+		}
+		
+	function selectNode(id) {
+		
+		var currnode = d3.selectAll(".node");
+       	currnode.classed("node_selected", function(d, i) {
+       					//don't remove class for nodes that are already selected
+       					if(this.classList.contains("node_selected")) {
+       						return true;
+       					}
+                       	return d.index === id;
+	               	 	});
+		
+		}
+
+     function tick() {
+         link.attr("x1", function(d) {
+             return d.source.x;
+         })
+             .attr("y1", function(d) {
+                 return d.source.y;
+             })
+             .attr("x2", function(d) {
+                 return d.target.x;
+             })
+             .attr("y2", function(d) {
+                 return d.target.y;
+             });
+
+         node.attr("cx", function(d) {
+             return d.x;
+         })
+             .attr("cy", function(d) {
+                 return d.y;
+             });
+     }
+
+     function mousedown() {
+         if (!mousedown_node && !mousedown_link) {
+             // allow panning if nothing is (being) selected
+             vis.call(d3.behavior.zoom().on("zoom"), rescale);
+             return;
+         }
+     }
+
+     function mousemove() {
+         if (!mousedown_node) return;
+     }
+
+     function mouseup() {
+         if (mousedown_node) {
+             // hide drag line
+
+             if (!mouseup_node) {
+                 // add node
+             }
+         }
+         // clear mouse event vars
+         resetMouseVars();
+     }
+
+     // rescale g
+     function rescale() {
+         trans = d3.event.translate;
+         scale = d3.event.scale;
+
+         vis.attr("transform",
+             "translate(" + trans + ")" + " scale(" + scale + ")");
+     }
+
+     function resetMouseVars() {
+         mousedown_node = null;
+         mouseup_node = null;
+         mousedown_link = null;
+         mousedown_mesh = null;
+         mouseup_mesh = null;
+     }
+
+     document.getElementById('colorby').addEventListener('change', function() {
+         colorby = this.value;
+         valmin = d3.min(function() {
+             var temp = [];
+             graph.nodes.forEach(function(d) {
+                 temp.push(d.props[colorby]);
+             });
+             return temp
+         }());
+         valmax = d3.max(function() {
+             var temp = [];
+             graph.nodes.forEach(function(d) {
+                 temp.push(d.props[colorby]);
+             });
+             return temp
+         }());
+         GraphColor.domain([valmin, valmax]);
+         node.transition()
+             .style("fill", function(d) {
+                 return GraphColor(d.props[colorby]);
+             })
+
+     });
+
+createSVG();
+
+
---
+++ webGLplot.js
@@ -0,0 +1,302 @@
+
+ // WebGL window
+
+        if (!Detector.webgl) Detector.addGetWebGLMessage();
+
+        var container, stats, controls;
+        var animation, rotate = true;
+        var raycaster, mouse = new THREE.Vector2(), INTERSECTED, prevMaterial;
+
+        var camera, cameraTarget, scene, renderer;
+
+        init();
+        animate();
+
+        function init() {
+
+            cont = d3.selectAll("#threeDplot"); //document.createElement('div');container
+            container = cont[0][0]; // dirty trick to get the DOM element
+            //document.body.appendChild(container);
+
+            window.GLwindowHeight = window.innerHeight-window.d3windowHeight;
+        window.GLwindowWidth
+
+            camera = new THREE.PerspectiveCamera(35, window.GLwindowWidth / (window.GLwindowHeight ), 1, 15);
+            camera.position.set(3, 0.15, 3);
+            cameraTarget = new THREE.Vector3(0, -0.25, 0);
+
+            scene = new THREE.Scene();
+            scene.fog = new THREE.Fog(0x72645b, 2, 15);
+
+
+            // Ground
+
+            var plane = new THREE.Mesh(new THREE.PlaneGeometry(40, 40), new THREE.MeshPhongMaterial({
+            //var plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(40, 40), new THREE.MeshPhongMaterial({
+                ambient: 0x999999,
+                color: 0x999999,
+                specular: 0x1F1F1F
+            }));
+            plane.rotation.x = -Math.PI / 2;
+            plane.position.y = -0.5;
+            //with receiveShadow it doesn't show; without it you can't see the cylinder
+            //scene.add(plane);
+
+            //plane.receiveShadow = true;
+
+            //var material = new THREE.MeshPhongMaterial( { ambient: 0x555555, color: 0xAAAAAA, specular: 0x111111, shininess: 200 } );
+            NormalMaterial = new THREE.MeshBasicMaterial({
+                wireframe: true,
+                wireframeLinewidth: 1,
+                transparent: true,
+                opacity: 0.1
+            });
+            HighlightMaterial = new THREE.MeshBasicMaterial({
+                wireframe: true,
+                wireframeLinewidth: 1,
+                transparent: true,
+                opacity: 1.0,
+                color: 0xdd0c24
+            });
+            HoverMaterial = new THREE.MeshBasicMaterial({
+                wireframe: true,
+                wireframeLinewidth: 1,
+                transparent: true,
+                opacity: 0.1,
+                color: 0x0000ff
+            });
+
+            meshes = [];
+            loader = new THREE.STLLoader();
+            
+
+
+            // Lights
+
+            scene.add(new THREE.AmbientLight(0x777777));
+
+            addShadowedLight(1, 1, 1, 0xffffff, 1.35);
+            addShadowedLight(0.5, 1, -1, 0xffaa00, 1);
+
+            // renderer
+
+            renderer = new THREE.WebGLRenderer({
+                antialias: true
+            });
+            renderer.setSize(window.GLwindowWidth, window.GLwindowHeight);
+
+            renderer.setClearColor(scene.fog.color, 1);
+
+            renderer.gammaInput = true;
+            renderer.gammaOutput = true;
+
+            renderer.shadowMapEnabled = true;
+            renderer.shadowMapCullFace = THREE.CullFaceBack;
+
+            container.appendChild(renderer.domElement);
+            
+            // camera controls
+            
+            controls = new THREE.OrbitControls(camera, renderer.domElement);
+            
+            // interaction with separate meshes
+            
+            raycaster = new THREE.Raycaster();
+            document.getElementById("threeDplot").addEventListener( 'mousemove', onMouseMove, false );
+            document.getElementById("threeDplot").addEventListener( 'click', onClick, false );
+            
+            // stats
+
+            stats = new Stats();
+            stats.domElement.style.position = 'absolute';
+            stats.domElement.style.top = '0px';
+            container.appendChild(stats.domElement);
+
+        }
+
+        function addShadowedLight(x, y, z, color, intensity) {
+
+            var directionalLight = new THREE.DirectionalLight(color, intensity);
+            directionalLight.position.set(x, y, z)
+            scene.add(directionalLight);
+
+            directionalLight.castShadow = true;
+            // directionalLight.shadowCameraVisible = true;
+
+            var d = 1;
+            directionalLight.shadowCameraLeft = -d;
+            directionalLight.shadowCameraRight = d;
+            directionalLight.shadowCameraTop = d;
+            directionalLight.shadowCameraBottom = -d;
+
+            directionalLight.shadowCameraNear = 1;
+            directionalLight.shadowCameraFar = 4;
+
+            directionalLight.shadowMapWidth = 1024;
+            directionalLight.shadowMapHeight = 1024;
+
+            directionalLight.shadowBias = -0.005;
+            directionalLight.shadowDarkness = 0.15;
+
+        }
+	
+	
+	
+		//interaction with separate meshes
+		function onMouseMove( event ) {
+
+			event.preventDefault();
+			
+			var x = event.clientX - document.getElementById("threeDplot").offsetLeft;
+			var y = event.clientY - document.getElementById("threeDplot").offsetTop;			
+
+			mouse.x = ( x / window.GLwindowWidth ) * 2 - 1;
+			mouse.y = - ( y / window.GLwindowHeight ) * 2 + 1;
+
+		}
+		
+		function onClick( event ) {
+
+			event.preventDefault();
+			
+			var vector, raycasterClick, intersectsClick, selected_id;
+			vector = new THREE.Vector3(mouse.x, mouse.y, 0.5).unproject(camera);
+			raycasterClick = new THREE.Raycaster(camera.position, vector.sub(camera.position).normalize() );
+			intersectsClick = raycasterClick.intersectObjects(scene.children);
+			
+			if (intersectsClick.length > 0) {
+			
+				selected_id = intersectsClick[0].object.realId;
+			
+				if (prevMaterial != HighlightMaterial) {
+					selectMesh(selected_id);
+					selectNode(selected_id);
+				}
+				else {
+					deselectMesh(selected_id);		
+					deselectNode(selected_id);						
+				}
+			}
+
+		}
+
+		function deselectMesh(id) {
+	                   	 	
+	        meshes[id].material = NormalMaterial;
+			prevMaterial = NormalMaterial;
+		}
+        
+        function selectMesh(id) {
+          	 	
+	        meshes[id].material = HighlightMaterial;
+			prevMaterial = HighlightMaterial;
+        }
+
+
+
+        function animate() {
+            animation = requestAnimationFrame(animate);
+
+            render();
+            stats.update();
+            controls.update();
+            updateTimers();
+
+        }
+        
+        
+        var timerx = Date.now() * 0.0002;
+        var timerz = timerx;
+        
+        //save the current camera position
+        function updateTimers() {
+        	timerx = Math.acos( (camera.position.x - 0.5) / 2);
+			timerz = Math.asin( (camera.position.z) / 2);
+		}
+        
+		function toggleAnimation() {
+			updateTimers();		
+			//at some point stops rotating :?	
+			rotate = !rotate;
+		}
+
+        function render() {
+			if (rotate) {
+				//better way to do this?
+	            timerx += 0.01;
+    	        timerz += 0.01;
+    	        camera.position.x = 0.5 + Math.cos(timerx) * 2;
+    	        camera.position.z = Math.sin(timerz) * 2;
+			}
+
+            camera.lookAt(cameraTarget);
+            
+
+            // interaction - find intersections
+
+			raycaster.setFromCamera( mouse, camera );
+			var intersects = raycaster.intersectObjects( scene.children );
+				
+			if ( intersects.length > 0 ) {
+				
+				if ( INTERSECTED != intersects[ 0 ].object ) {
+
+					if ( INTERSECTED ) INTERSECTED.material = prevMaterial;
+
+						INTERSECTED = intersects[ 0 ].object;
+						prevMaterial = INTERSECTED.material;
+						INTERSECTED.material = HoverMaterial;
+
+					}
+
+				} else {
+
+					if ( INTERSECTED ) INTERSECTED.material = prevMaterial;
+
+					INTERSECTED = null;
+
+				}
+				
+            renderer.render(scene, camera);
+
+        }
+        
+
+
+        function loadSTLs(aList) {
+        var a = 0;
+            aList.forEach(function(filepath) {
+				loader.load( filepath, function ( geometry ) {
+
+                    var material = NormalMaterial;
+                    var mesh = new THREE.Mesh( geometry, material );
+                    
+                    mesh.position.set(0.0, -0.4, 0.0);
+                	mesh.rotation.set(-Math.PI / 2, 0, 0);
+                	mesh.scale.set(6, 6, 6);
+                	
+                	mesh.castShadow = true;
+            		//mesh.receiveShadow = true;
+            		
+            		//actual id kept in object - as opposed to scene id
+            		mesh.realId = a;
+            		a+=1;
+            		meshes.push(mesh);
+                    
+                    scene.add( mesh );
+                    
+                } );
+             } );
+             
+
+         }
+
+    function removeSTLs(){
+        meshes.forEach(function(mesh){ scene.remove(mesh);});
+    }
+
+function updateGLview() {
+    camera.aspect = window.GLwindowWidth / (window.GLwindowHeight);
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.GLwindowWidth, window.GLwindowHeight);
+}
